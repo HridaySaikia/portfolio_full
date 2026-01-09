d4fa3c679b6d0761436c3e794215b98fb675068a
HridaySaikia <hridayanandasaikia96@gmail.com>
2026-01-09 17:22:07 +0530

    Update fetcher.ts

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,16 +1,13 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
 // utils/fetcher.ts
+
 export const fetcher = async (
   path: string,
   options: RequestInit = {}
 ): Promise<Response> => {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
-
-  return fetch(`${baseUrl}${path}`, {
+  return fetch(path, {
     ...options,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       ...options.headers,
     },
   });
