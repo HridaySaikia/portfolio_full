f878670e8e5ccdabe590afad028b1451b1e2fba9
HridaySaikia <hridayanandasaikia96@gmail.com>
2026-01-09 17:54:20 +0530

    Update getProfile.ts

--- lib/getProfile.ts
+++ lib/getProfile.ts
@@ -1,9 +1,19 @@
-import connectDB from "@/lib/mongodb";
-import Profile from "@/models/Profile";
+import { connectDB } from "@/lib/db";
+import { Profile } from "@/lib/models/Profile";
 
-export async function getProfile() {
+export async function getProfileFromDB() {
   await connectDB();
   const profile = await Profile.findOne().lean();
+  return JSON.parse(JSON.stringify(profile));
+}
+
+export async function upsertProfile(data: any) {
+  await connectDB();
+  const profile = await Profile.findOneAndUpdate(
+    {},
+    data,
+    { upsert: true, new: true }
+  ).lean();
 
   return JSON.parse(JSON.stringify(profile));
 }
