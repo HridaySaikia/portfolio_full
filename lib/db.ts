cf04c510af7de35902ee9597b36eb508b65c5511
HridaySaikia <hridayanandasaikia96@gmail.com>
2026-01-10 20:07:52 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,32 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
 
 export async function connectDB() {
-  if (mongoose.connections[0].readyState) return;
+  if (cached.conn) return cached.conn;
 
-  await mongoose.connect(MONGODB_URI, {
-    dbName: "portfolio",
-  });
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "portfolio",
+        bufferCommands: false, // 🔥 critical
+      })
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
