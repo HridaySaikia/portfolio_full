bdcb4641aeb4439917fd086b5fa0cf30bdfdb297
HridaySaikia <hridayanandasaikia96@gmail.com>
2026-01-10 19:45:25 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
-   images: {
-    domains: ['res.cloudinary.com'], // ✅ allow Cloudinary
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
